b08c3999c00306eb884a47d6477590d2177eef3a
Spock <spock.hsueh@kkday.com>
2022-10-31 17:56:30 +0800

    add init and new func

--- Sources/MyLibrary3/MyLibrary3.swift
+++ Sources/MyLibrary3/MyLibrary3.swift
@@ -1,6 +1,15 @@
 public struct MyLibrary3 {
     public private(set) var text = "Hello, World 3"
+    
+    private var name: String
+    private var age: Int
 
-    public init() {
+    public init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+    
+    public func differentProfile() -> String {
+        return "\(name) \(age) from pod3"
     }
 }
